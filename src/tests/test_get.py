726daf885a789877b6d7d9a8c48af2ae362e7c81
Mathias V. Nielsen <hube321@gmail.com>
2020-09-25 17:49:44 +0200

    Changed so all tests read from config.ini

--- src/tests/test_get.py
+++ src/tests/test_get.py
@@ -1,18 +1,21 @@
-import requests
 import unittest
+import requests
 
-HOST = '127.0.0.1'
-PORT = 80
+from src.modules import Config
 
 
 class Get(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.config = Config()
 
     def test_get_content(self):
         """
         Test that we get content back when requesting default file
         :return:
         """
-        req = requests.get("http://127.0.0.1/")
+        req = requests.get("http://{}:{}/".format(self.config.options.get("Server", "ListenAddress"), self.config.options.get("Server", "Port")))
 
         self.assertTrue(req.text)
 
@@ -21,7 +24,7 @@ class Get(unittest.TestCase):
         Test that we cannot access paths outside of the working directory
         :return:
         """
-        req = requests.get("http://127.0.0.1/../README.md")
+        req = requests.get("http://{}:{}/../README.md".format(self.config.options.get("Server", "ListenAddress"), self.config.options.get("Server", "Port")))
 
         self.assertEqual(req.status_code, 404)
 
